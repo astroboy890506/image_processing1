c4500750e0b7e81d4efe1aaddc72bba207796084
astroboy890506 <97178858+astroboy890506@users.noreply.github.com>
2023-08-24 16:16:24 +0800

    Add files via upload

---
+++ image.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+# Rest of your code as it is
+
+def main():
+    st.set_page_config(
+        page_title="Image Filtering App",
+        page_icon=":camera:",
+        layout="centered",
+        initial_sidebar_state="expanded"
+    )
+
+    st.title("Image Filtering")
+
+    # Upload an image from the user's local PC
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        img_array = np.fromstring(uploaded_file.read(), np.uint8)
+        uploaded_image = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        gray_uploaded_image = cv2.cvtColor(uploaded_image, cv2.COLOR_BGR2GRAY)
+
+        # Display the uploaded image
+        st.subheader("Uploaded Image")
+        st.image(cv2.cvtColor(gray_uploaded_image, cv2.COLOR_BGR2RGB), use_column_width=True)
+
+        # Display filter options
+        filter_type = st.selectbox("Select a filter", ["Averaging", "Gaussian", "Median"])
+
+        if filter_type == "Averaging":
+            # Apply Averaging filtering
+            blurAverage = cv2.blur(gray_uploaded_image, (7, 7))
+            st.subheader("Averaging Filtering")
+            st.image(blurAverage, cmap='gray', use_column_width=True)
+
+        elif filter_type == "Gaussian":
+            # Apply Gaussian filtering
+            blurGauss = cv2.GaussianBlur(gray_uploaded_image, (5, 5), 0)
+            st.subheader("Gaussian Filtering")
+            st.image(blurGauss, cmap='gray', use_column_width=True)
+
+        elif filter_type == "Median":
+            # Apply Median filtering
+            blurMedian = cv2.medianBlur(gray_uploaded_image, 3)
+            st.subheader("Median Filtering")
+            st.image(blurMedian, cmap='gray', use_column_width=True)
+
+if __name__ == "__main__":
+    main()
