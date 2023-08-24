0824a240735f7796dae999ac76eb66fd62c406b7
astroboy890506 <97178858+astroboy890506@users.noreply.github.com>
2023-08-24 20:46:55 +0800

    Update image.py

--- image.py
+++ image.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
-
-# Rest of your code as it is
 
 def main():
     st.set_page_config(
@@ -21,32 +18,31 @@ def main():
     if uploaded_file is not None:
         img_array = np.fromstring(uploaded_file.read(), np.uint8)
         uploaded_image = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        gray_uploaded_image = cv2.cvtColor(uploaded_image, cv2.COLOR_BGR2GRAY)
 
         # Display the uploaded image
         st.subheader("Uploaded Image")
-        st.image(cv2.cvtColor(gray_uploaded_image, cv2.COLOR_BGR2RGB), use_column_width=True)
+        st.image(uploaded_image, channels="BGR", use_column_width=True)
 
         # Display filter options
         filter_type = st.selectbox("Select a filter", ["Averaging", "Gaussian", "Median"])
 
         if filter_type == "Averaging":
             # Apply Averaging filtering
-            blurAverage = cv2.blur(gray_uploaded_image, (7, 7))
+            blurAverage = cv2.blur(uploaded_image, (7, 7))
             st.subheader("Averaging Filtering")
-            st.image(blurAverage, cmap='gray', use_column_width=True)
+            st.image(blurAverage, channels="BGR", use_column_width=True)
 
         elif filter_type == "Gaussian":
             # Apply Gaussian filtering
-            blurGauss = cv2.GaussianBlur(gray_uploaded_image, (5, 5), 0)
+            blurGauss = cv2.GaussianBlur(uploaded_image, (5, 5), 0)
             st.subheader("Gaussian Filtering")
-            st.image(blurGauss, cmap='gray', use_column_width=True)
+            st.image(blurGauss, channels="BGR", use_column_width=True)
 
         elif filter_type == "Median":
             # Apply Median filtering
-            blurMedian = cv2.medianBlur(gray_uploaded_image, 3)
+            blurMedian = cv2.medianBlur(uploaded_image, 3)
             st.subheader("Median Filtering")
-            st.image(blurMedian, cmap='gray', use_column_width=True)
+            st.image(blurMedian, channels="BGR", use_column_width=True)
 
 if __name__ == "__main__":
     main()
