aac4a55094be7e75fb6f863da6c13be1c2c7c442
astroboy890506 <97178858+astroboy890506@users.noreply.github.com>
2023-08-24 20:36:49 +0800

    Update image.py

--- image.py
+++ image.py
@@ -21,30 +21,31 @@ def main():
     if uploaded_file is not None:
         img_array = np.fromstring(uploaded_file.read(), np.uint8)
         uploaded_image = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        gray_uploaded_image = cv2.cvtColor(uploaded_image, cv2.COLOR_BGR2GRAY)
+        # gray_uploaded_image = cv2.cvtColor(uploaded_image, cv2.COLOR_BGR2GRAY)
 
         # Display the uploaded image
         st.subheader("Uploaded Image")
-        st.image(cv2.cvtColor(gray_uploaded_image, cv2.COLOR_BGR2RGB), use_column_width=True)
+        st.image(cv2.cvtColor(uploaded_image, cv2.COLOR_BGR2RGB), use_column_width=True)
+        #st.image(cv2.cvtColor(gray_uploaded_image, cv2.COLOR_BGR2RGB), use_column_width=True)
 
         # Display filter options
         filter_type = st.selectbox("Select a filter", ["Averaging", "Gaussian", "Median"])
 
         if filter_type == "Averaging":
             # Apply Averaging filtering
-            blurAverage = cv2.blur(gray_uploaded_image, (5,5))
+            blurAverage = cv2.blur(uploaded_image, (5,5))
             st.subheader("Averaging Filtering")
             st.image(blurAverage, use_column_width=True)
 
         elif filter_type == "Gaussian":
             # Apply Gaussian filtering
-            blurGauss = cv2.GaussianBlur(gray_uploaded_image, (5, 5), 0)
+            blurGauss = cv2.GaussianBlur(uploaded_image, (5, 5), 0)
             st.subheader("Gaussian Filtering")
             st.image(blurGauss, use_column_width=True)
 
         elif filter_type == "Median":
             # Apply Median filtering
-            blurMedian = cv2.medianBlur(gray_uploaded_image, 5)
+            blurMedian = cv2.medianBlur(uploaded_image, 5)
             st.subheader("Median Filtering")
             st.image(blurMedian, use_column_width=True)
 
