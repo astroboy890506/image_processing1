80daa91f7345fade4b4f4e9761c33c0c581d007e
astroboy890506 <97178858+astroboy890506@users.noreply.github.com>
2023-08-25 19:57:26 +0800

    Update image.py

--- image.py
+++ image.py
@@ -24,30 +24,25 @@ def main():
         filter_type = st.sidebar.selectbox("Select a filter", ["Averaging", "Gaussian", "Median"])
 
         window_size = st.sidebar.slider("Window Size", min_value=1, max_value=50, value=25)
-        
-        # Allow users to adjust the size of the uploaded image
-        image_scaling_factor = st.sidebar.slider("Image Scaling Factor", min_value=0.1, max_value=2.0, value=1.0)
-
-        scaled_image = cv2.resize(uploaded_image, None, fx=image_scaling_factor, fy=image_scaling_factor)
 
         # Allow users to adjust the displayed image width
         image_width = st.slider("Adjust Image Width", min_value=100, max_value=800, value=500)
 
         if filter_type == "Averaging":
             # Apply Averaging filtering
-            blurAverage = cv2.blur(scaled_image, (window_size, window_size))
+            blurAverage = cv2.blur(uploaded_image, (window_size, window_size))
             st.subheader("Averaging Filtering")
             st.image(blurAverage, channels="BGR", width=image_width)
 
         elif filter_type == "Gaussian":
             # Apply Gaussian filtering
-            blurGauss = cv2.GaussianBlur(scaled_image, (window_size, window_size), 0)
+            blurGauss = cv2.GaussianBlur(uploaded_image, (window_size, window_size), 0)
             st.subheader("Gaussian Filtering")
             st.image(blurGauss, channels="BGR", width=image_width)
 
         elif filter_type == "Median":
             # Apply Median filtering
-            blurMedian = cv2.medianBlur(scaled_image, window_size)
+            blurMedian = cv2.medianBlur(uploaded_image, window_size)
             st.subheader("Median Filtering")
             st.image(blurMedian, channels="BGR", width=image_width)
 
